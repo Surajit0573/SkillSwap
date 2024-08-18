153f8d06ba7b44d8cb9fda7bedae259c13422ccc
Surajit Maity <sibu23122003@gmail.com>
2024-08-18 15:18:02 +0530

    Loader added in Home Page

--- Client/src/Component/Home/Content.jsx
+++ Client/src/Component/Home/Content.jsx
@@ -1,7 +1,7 @@
 import Card from './Card';
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-
+import GridLoader from "react-spinners/GridLoader";
 export default function Content() {
     const [header, setHeader] = useState("All Online Classes");
     const location = useLocation();
@@ -9,9 +9,10 @@ export default function Content() {
     let catId = null;
 
     const [data, setData] = useState([]);
-    
+    const [loading, setLoading] = useState(true); // Add a loading state
     useEffect(() => {
         async function fetchData() {
+            setLoading(true);
             try {
                 catId = location.state?.catId;
                 let url = `${import.meta.env.VITE_URL}/api/courses`;
@@ -32,11 +33,26 @@ export default function Content() {
                 }
             } catch (err) {
                 console.error('Error fetching data:', err);
+            } finally {
+                setLoading(false); // Set loading to false after fetching data
             }
         }
         fetchData();
     }, [location, navigate]);
 
+    if (loading) {
+        return (
+            <GridLoader
+                color={'#0059ef'}
+                loading={true}
+                cssOverride={override}
+                size={30}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        );
+    }
+    
     return (
         <div className="flex flex-col text-left h-[70vh] w-[80vw] overflow-y-scroll bg-gray-900 text-gray-100 p-4 rounded-md shadow-md">
             <p className="font-semibold text-3xl mb-4">{header}</p>
